f3c8848bd55c45bd8c941f7640a3e956176b97ce
isabelle41 <isabelle.joyce@mail.utoronto.ca>
2026-02-06 01:11:55 -0500

    1. Added startUpRoutine logic for turning to furthest distance. 2. Added isStuck logic to check if the robot has been not maving a lot for a while and return to the startup routine. 3. Added comments that should be addressed tomorrow regarding syntax and coding logic.

--- contest1.cpp
+++ contest1.cpp
@@ -59,7 +59,7 @@ public:
         // Initialize variables
         start_time_ = this->now(); //define start time
         startup = true; //boolean to indicate control loop is executing the first time
-        callstartupRoutine = true;  //why do we have this and line 61???
+        callstartupRoutine = true;  // boolean to indicate whether to call startup routines
         angular_ = 0.0; //initialize angular velocity
         linear_ = 0.0;  //initialize linear velocity
         pos_x_ = 0.0; //initialize x position
@@ -71,11 +71,10 @@ public:
         desiredAngle_ = 5;
         minimumFrontDistance = 0.5; 
         isTurning = false;
-        initialGoalYaw = 0.0;
         enterBumperHandling = false;
-        currentYaw = 0.0; //?????
-        currentX = 0.0; //?????
-        currentY = 0.0; //?????
+        currentYaw = 0.0; // set to current yaw when setPositions() is called - used for some handler routines (e.g. startup routine)
+        currentX = 0.0; // set to current X position when setPositions() is called - used for some handler routines (e.g. startup routine)
+        currentY = 0.0; // set to current Y position when setPositions() is called - used for some handler routines (e.g. startup routine)
         anyBumperPressed = false; 
         front_distance_ = std::numeric_limits<float>::infinity();
         right_distance_ = std::numeric_limits<float>::infinity();
@@ -118,12 +117,6 @@ private:
                 minLaserDist_ = std::min(minLaserDist_, laserRange_[laser_idx]);
             }
         }
-
-        int front_idx_from_scan_ = static_cast<int>(
-            (0.0 - scan->angle_min) / scan->angle_increment
-        ); //what does this do???
-
-        //front_idx_ = front_idx_from_scan_;
 
         int front_idx_ = nLasers_ / 4; //find index of front obstacle distance measurement
         int back_idx_ = nLasers_ * 3/4 ; //find index of back obstacle distance measurement
@@ -175,8 +168,25 @@ private:
     #pragma region Routines
     void startupRoutine()
     {
-        // Startup routine to orient robot towards direction of maximum laser distance and drive to within 50cm of an obstacle
         RCLCPP_INFO(this -> get_logger(),"WE'RE IN STARTUP ROUTINE");
+        // Startup routine to orient robot towards direction of maximum laser distance and drive to within 50cm of an obstacle
+
+        // Turn robot towards furthest wall
+        float distanceDifference = abs(front_distance_ - max_element); // calculate distance difference - max_element defined in control loop
+        if (distanceDifference > 0.01)  // if not yet facing furthest wall
+        { 
+            RCLCPP_INFO(this->get_logger(), "turning to face furthest wall");
+            linear_ = 0.0;
+            angular_ = turnClockwise ? 0.1 : -0.1; // turn in specified direction - turnClockwise defined in control loop
+        }
+        else if (front_distance_ > 0.5) // drive facing furthest wall until wall is within 50cm or less
+        {
+            RCLCPP_INFO(this->get_logger(), "facing furthest wall, moving to approach");
+            linear_ = 0.2;
+            angular_ = 0.0; 
+        }
+        else { callstartupRoutine = false; } // Exit startup routine
+        
         
         // Move forward until minimum laser distance is 50cm or less
         if (front_distance_ > minimumFrontDistance)
@@ -196,10 +206,10 @@ private:
         RCLCPP_INFO(this -> get_logger(),"front_distance: %.2f , left_distance: %.2f, right_distance: %.2f", front_distance_, left_distance_, right_distance_);
 
         // Determine if robot is close to an obstacle in front and needs to turn
-        bool turnClockwise = false; // true = turn right, false = turn left
+        bool _turnClockwise = false; // true = turn right, false = turn left
         if (front_distance_ <= 0.5 && !isTurning)
         {
-            if (left_distance_ <= 0.5 && right_distance_ > 0.01) { turnClockwise = true; } // turn right if left wall is also close
+            if (left_distance_ <= 0.5 && right_distance_ > 0.01) { _turnClockwise = true; } // turn right if left wall is also close
             isTurning = true;
         }
         
@@ -208,7 +218,7 @@ private:
             linear_ = 0.2;
             angular_ = 0.0;
         }
-        else { turnRobot(turnClockwise); } // robot is turning
+        else { turnRobot(_turnClockwise); } // robot is turning
             
         return; 
     }
@@ -217,19 +227,19 @@ private:
 
     #pragma region Utilities
 
-    void turnRobot(bool turnClockwise)
+    void turnRobot(bool _turnClockwise)
     {
         // Turn robot in specified direction until min side laser distance matches min laser distance from filteredLaserRange_
-        RCLCPP_INFO(this->get_logger(), "Turning robot %s", turnClockwise ? "clockwise" : "counter-clockwise");
+        RCLCPP_INFO(this->get_logger(), "Turning robot %s", _turnClockwise ? "clockwise" : "counter-clockwise");
 
         // Find minimum distance from filtered laser ranges
         float min_element = *std::min_element(begin(filteredLaserRange_), end(filteredLaserRange_)); // find minimum distance from filtered laser ranges
-        float distanceDifference = turnClockwise ? abs(left_distance_ - min_element) : abs(right_distance_ - min_element); // calculate distance difference based on turn direction
+        float distanceDifference = _turnClockwise ? abs(left_distance_ - min_element) : abs(right_distance_ - min_element); // calculate distance difference based on turn direction
 
         if (distanceDifference > 0.01)  // if not yet reached target rotation
         { 
             linear_ = 0.0;
-            angular_ = turnClockwise ? 0.1 : -0.1; // turn in specified direction
+            angular_ = _turnClockwise ? 0.1 : -0.1; // turn in specified direction
         }
         else // reached target rotation
         {
@@ -266,11 +276,15 @@ private:
                 pressed_bumper = key; // store the key of the pressed bumper
                 RCLCPP_INFO(this-> get_logger(),"Pressed bumper: %s", pressed_bumper.c_str());
             }
-        }
+        } // CODE COMMENT: bumpers will not always have been pressed when we enter this function, so if we want to save which specific bumper was pressed, we should make a global variable and probably set it in isBumperPressed(),/n
+        //      so that it is set when the bumper is first pressed, and not overwritten with every iteration.
+
+        // CODE COMMENT: Everything is called over and over again as the control loop is executed. This means that the last linear_ and angular_ values that are set in each loop will overwrite any previous logic. /n
+        //         So if you set linear_ as a negative value in moveBack(), when you exit moveBack(), linear_ and angular_ will be set to the values following the moveBack() function, and the logic from moveBack() is overwritten.
         if(pressed_bumper == "bump_from_left" || pressed_bumper == "bump_left")
         {
             RCLCPP_INFO(this -> get_logger(),"Left bumper was pressed - turn right");
-            moveBack(0);
+            moveBack(0); // CODE COMMENT: input to moveBack() should probably be a global variable
             linear_ = 0.0;
             angular_ = -0.2; // turn right
         }
@@ -316,19 +330,25 @@ private:
         return; */
         return;
     }
+    
     void moveBack(int state_){
         if (state_ ==0){
-            if(start_pos_x ==0.0 && start_pos_y_ ==0.0) {
+            if(start_pos_x ==0.0 && start_pos_y_ ==0.0) { 
                 start_pos_x = pos_x_;
                 start_pos_y_ = pos_y_;
+                // CODE COMMENT
+                // start_pos_x and start_pos_y_ are initialized to 0 with every iteration of the control loop, so this condition will always be true, and therefore when /n
+                //   these variables are accessed later, they will always equal pos_x_ and pos_y_. If you want to access the position of the robot at the moment the bumper was /n
+                //   pressed without updating every time, you can use currentX and currentY, which are set to the position of the robot at the moment the bumper is pressed in /n
+                //   the setCurrentPositions() function (check line 472), and are not updated until the next time a bumper is pressed. Otherwise, you could just used pos_x_ and pos_y_ directly.
             }
         }
 
-        double distance_travelet = std::sqt(
+        double distance_traveled = std::sqrt(
             std::pow(pos_x_ - start_pos_x, 2) + std::pow(pos_y_ - start_pos_y_, 2)
         );
 
-        if(distance_traveled <target_distance)
+        if(distance_traveled <target_distance) //CODE COMMENT: this is a global variable but it's only used here. If we leave it as a global variable, we should set it at the top with the other global variables, but since it's only used here, to just use 0.05 and not make it a variable, or make it local.
         {
             linear_ = -0.1; // back up
             angular_ = 0.0;
@@ -336,7 +356,7 @@ private:
         }
         else {
             RCLCPP_INFO(this -> get_logger(),"Finished backing up, distance traveled: %.2f", distance_traveled);
-            state_ = 1;
+            state_ = 1; // CODE COMMENT: this variable is always 0 because moveBack is always called with 0 as an argument. This won't update the value. We can make a global variable for state if we want to keep track of it across function calls.
             linear_ = 0.0;
             angular_ = 0.0;
         }
@@ -367,6 +387,7 @@ private:
 
     void setCurrentPositions()
     {
+        // CODE COMMENT: NOT USED RIGHT NOW - MAY BE USEFUL FOR BUMPER HANDLING
         // Set current positions and yaw for bumper handling routine
         currentX = pos_x_;
         currentY = pos_y_;
@@ -382,11 +403,11 @@ private:
         double random_angle = distribution(generator);
         RCLCPP_INFO(this->get_logger(), "Randomly turning to angle: %f radians", random_angle);
         
-        static bool RandomTurn = false;
-        static double target_yaw = 0.0;
+        static bool RandomTurn = false; // CODE COMMENT: this variable is always false because it's defined in the function. If we want to keep track of it across function calls, we should make it a global variable.
+        static double target_yaw = 0.0; // CODE COMMENT: this variable is always 0 because it's defined in the function. If we want to keep track of it across function calls, we should make it a global variable.
         
         if (front_distance <= 0.5 && !RandomTurn) {
-            linear_ = -0.1; // back up if too close to obstacle
+            linear_ = -0.1; // back up if too close to obstacle // CODE COMMENT: this is overwritten in the next iterations a few miliseconds later, so might as well be 0
             angular_ = 0.0;
             RandomTurn = true;
             
@@ -407,6 +428,41 @@ private:
             }
         }
 
+    }
+
+    bool isStuck()
+    {
+        if (!stuck_timer_active_) 
+        {
+            stuck_start_time_ = this->now();
+            stuck_start_x_ = pos_x_;
+            stuck_start_y_ = pos_y_;
+            stuck_start_yaw_ = yaw_;
+            stuck_timer_active_ = true;
+            return false;
+        }
+
+        double dist = std::hypot(pos_x_ - stuck_start_x_, pos_y_ - stuck_start_y_);
+        double yaw_change = std::abs(normalizeAngle(yaw_ - stuck_start_yaw_));
+        double time_elapsed = (this->now() - stuck_start_time_).seconds();
+
+        // Reset timer if robot is clearly moving forward
+        if (dist > STUCK_DISTANCE_THRESH) 
+        {
+            stuck_timer_active_ = false;
+            return false;
+        }
+
+        // If turning in place for too long → stuck
+        if (yaw_change > STUCK_YAW_THRESH && time_elapsed > STUCK_TIME_THRESH) {
+            RCLCPP_WARN(this->get_logger(),
+                "Robot appears stuck (%.2fm moved, %.1f deg turned over %.1fs)",
+                dist, rad2deg(yaw_change), time_elapsed);
+            stuck_timer_active_ = false;
+            return true;
+        }
+
+        return false;
     }
 
     #pragma endregion Utilities
@@ -442,6 +498,17 @@ private:
         // Define filteredLaserRange array with values from laserRange_ above 20cm
         createLasersArray(); 
 
+        // check if robot is stuck, and if yes, call isStuck and reset handling so that robot goes back to startup routine
+        if (isStuck()) 
+        {
+            RCLCPP_WARN(this->get_logger(), "Resetting to startup behavior");
+            startup = true;
+            callstartupRoutine = true;
+            isTurning = false;
+            enterBumperHandling = false;
+
+            filteredLaserRange_.clear();
+        }
         //set start x and y position equal to 0
         start_pos_x = 0.0;
         start_pos_y_ = 0.0;
@@ -456,6 +523,7 @@ private:
             enterBumperHandling = true;   
             setCurrentPositions();
         }
+        // CODE COMMENT: this logic should be implemented somewhere else in the code. The linear and angular velocities will be overwritten before they are published. 
         else if (min_element(filteredLaserRange_.begin(), filteredLaserRange_.end()) >= minLaserDist_) //checks to see where the closest obstacle is located, and orients the robot away from it if within a certain distance threshold
         {
             obstacle_idx_ = min_element(filteredLaserRange_.begin(), filteredLaserRange_.end()) - filteredLaserRange_.begin();
@@ -477,15 +545,20 @@ private:
             
         }
         else if (startup) // if true (first loop), set initial goal yaw and define current positions and set to false
-        {
-            initialGoalYaw = yaw_;
-            setCurrentPositions();
+        {            
+            // Find index of maximum distance from filtered laser ranges
+            max_element = *std::max_element(begin(filteredLaserRange_), end(filteredLaserRange_)); // find maximum distance from filtered laser ranges
+            max_element_idx = std::distance(filteredLaserRange_.begin(), std::max_element(filteredLaserRange_.begin(), filteredLaserRange_.end())); // find index of maximum distance
+
+            // Determine turn direction based on index of maximum distance
+            turnClockwise = max_element_idx < (filteredLaserRange_.size() / 2); // true = turn right, false = turn left
             startup = false;
         }
 
         // ENTER ROUTINE BASED ON CONDITIONS //
         if (enterBumperHandling)  { bumperPressedHandling(); } // handle bumper pressed event
         else if (callstartupRoutine) { startupRoutine(); } // perform startup routine (orient robot to face furthest wall & approach)
+        else if (seconds_elasped > 360) { randomSearchAlgorithm(); } // perform random search algorithm after 6 minutes have elapsed
         else { wallFollowingRoutine(); } // perform wall following routine
 
         // SET AND PUBLISH VELOCITY COMMAND //
@@ -527,7 +600,6 @@ private:
 
     bool startup;
     bool callstartupRoutine;
-    float initialGoalYaw;
 
     bool enterBumperHandling;
     bool anyBumperPressed;
@@ -538,6 +610,9 @@ private:
     float currentYaw;
     float currentX;
     float currentY;
+    float max_element;
+    int max_element_idx;
+    bool turnClockwise;
 
     float front_distance_;
     float right_distance_;
@@ -550,6 +625,19 @@ private:
     float distance_traveled;
     float target_distance; // distance to move back when bumper is pressed
     std::string pressed_bumper;
+
+    // Stuck detection
+    rclcpp::Time stuck_start_time;
+    double stuck_start_x_;
+    double stuck_start_y_;
+    double stuck_start_yaw_;
+    bool stuck_timer_active_ = false;
+
+    // Tunables
+    const double STUCK_DISTANCE_THRESH = 0.15;   // meters
+    const double STUCK_YAW_THRESH = deg2rad(90); // radians
+    const double STUCK_TIME_THRESH = 8.0;        // seconds
+
 };
 
 int main(int argc, char** argv)
